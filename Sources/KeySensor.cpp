4221271c6586d24fda9c0cb9bd841611f56e5258
Strabi <balazs.strahl@gmail.com>
2021-05-04 20:47:02 +0200

    gombok kezelése, tesztkóddal

--- Sources/KeySensor.cpp
+++ Sources/KeySensor.cpp
@@ -21,6 +21,58 @@ KeySensor::~KeySensor()
 {
 }
 
+typedef void KeyFunciton(void);
+
+typedef struct
+{
+	UINT8 key;
+	KeyFunciton *function;
+	UINT8 state;
+}KeyAction;
+
+void f1(void)
+{
+	printf("---- %s\n", __FUNCTION__);
+}
+void f2(void)
+{
+	printf("---- %s\n", __FUNCTION__);
+}
+void f3(void)
+{
+	printf("---- %s\n", __FUNCTION__);
+}
+void f4(void)
+{
+	printf("---- %s\n", __FUNCTION__);
+}
+
+KeyAction keyAction[] = 
+{
+	{VK_UP,		f1,		0},
+	{VK_DOWN,	f2,		0},
+	{VK_LEFT, 	f3,		0},
+	{VK_RIGHT,	f4,		0}
+};
+
+void executeKeyAction(UINT8 key, ButtonState buttonState)
+{
+	for (auto &&i : keyAction)
+	{
+		if(i.key == key)
+		{
+			i.state = ((i.state & 0x01) << 1) | buttonState;
+
+			if(i.state == BUTTON_PRESSED)
+			{
+				i.function();
+			}
+
+			return;
+		}
+	}
+}
+
 void KeySensor::ValueChanged(int argc, void** argv)
 {
 	static UINT8 upKey = 0;
@@ -39,18 +91,24 @@ void KeySensor::ValueChanged(int argc, void** argv)
 	switch(vk)
 	{
 		case VK_UP:
+		case VK_DOWN:
+		case VK_LEFT:
+		case VK_RIGHT:
 		{
+			executeKeyAction(vk, buttonState);
+			/*
 			upKey = ((upKey & 0x01) << 1) | buttonState;
 			printf("VK_UP: %d\n", upKey);
+
+			if(upKey == BUTTON_PRESSED)
+			{
+				printf("press\n");
+			}	
+			*/
 		} break;
 
 		default:
 		{		
 		}
 	}
-
-	if(upKey == BUTTON_PRESSED)
-	{
-		printf("press\n");
-	}	
 }
